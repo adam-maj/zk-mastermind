dba82453f8dccfbd6ef985a74b5330e6a059c21d
adam-maj <mr.adam.maj@gmail.com>
2022-11-13 15:15:16 -0500

    Update smart contract

--- scripts/build-contract.mjs
+++ scripts/build-contract.mjs
@@ -2,7 +2,7 @@ import { execSync } from "child_process";
 
 const commands = [
   `rm -rf contracts && mkdir contracts`,
-  `snarkjs zkey export solidityverifier public/keys/circuit_final.zkey contracts/Verifier.sol -v`,
+  `snarkjs zkey export solidityverifier circuits/mastermind/keys/circuit_final.zkey contracts/Verifier.sol -v`,
 ];
 
 for (const command of commands) {
